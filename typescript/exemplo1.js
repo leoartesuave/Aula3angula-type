0d895c96f5b52c83b83878fb13d1006a59fb3e56
leoartesuave <113612294+leoartesuave@users.noreply.github.com>
2023-07-10 11:22:40 -0300

    Add files via upload

---
+++ typescript/exemplo1.js
@@ -0,0 +1,8 @@
+var nome = "leo";
+var idade = 18;
+if (idade >= 18) {
+    console.log("Pode ir a festa");
+}
+else {
+    console.log("Não pode ir a festa");
+}
---
+++ typescript/exemplo1.ts
@@ -0,0 +1,8 @@
+var nome:string = "leo";
+var idade:number = 18;
+
+if(idade >= 18){
+    console.log("Pode ir a festa");
+}else{
+    console.log("Não pode ir a festa");
+}
